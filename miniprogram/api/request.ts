05b4119b5ac885b6e075f2175cced9ca97074710
Nicholas <zhangchangsheng01@gmail.com>
2022-01-09 15:27:51 +0800

    init

---
+++ miniprogram/api/request.ts
@@ -0,0 +1,21 @@
+const baseUrl = 'https://music-server-1542636-1255509607.ap-shanghai.run.tcloudbase.com'
+
+export const get = (url:string) =>{
+  wx.showLoading({
+    title:'加载中'
+  })
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url:baseUrl+url,
+      method:'GET',
+      success:(res)=>{
+        resolve(res)
+      },
+      fail: reject,
+      complete:()=>{
+        wx.hideLoading()
+      }
+    })
+  })
+
+}
---
+++ miniprogram/pages/index/index.ts
@@ -0,0 +1,14 @@
+// index.ts
+
+import { sayHello } from "../../api/hello"
+
+Page({
+  data: {
+    message:'Hello World'
+  },
+  hi(){
+    sayHello().then(res=>{
+      console.log(res)
+    })
+  }
+})
